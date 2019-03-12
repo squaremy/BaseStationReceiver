8a0dba7b883faa03149f0ac86f13c4092803ea22
Jordan <martinj1@franklinacademy.org>
2019-03-12 19:36:46 -0400

    added checksum and tuned down graph accuracy

--- main.py
+++ main.py
@@ -12,60 +12,58 @@ style.use("ggplot")
 ###
 # Program: Arduino communication
 # Author: Jordan Martin
-# Version: 0.2
+# Version: 1.0
 # Date Created: 8 March 2019
 # Description: Used to communicate between the arduinos and the computer... will eventually graph real time data
 # Last Edited By: Jordan Martin
-# Last Edited: 11 March 2019
-# Reason Edited: GPS graph working as intended, need to add filter for garbled data
+# Last Edited: 12 March 2019
+# Reason Edited: Weird data sort of filtered... added checksum
 ###
 #global variables here
-x = []
-y = []
+x = [] # list of x values
+y = [] # list of y values
 
 # custom classes/functions here
 class ArduinoCommunicator(): # class to communicate with plugged in arduino through serial monitor
     def __init__(self, port):
-        self.ser = serial.Serial(port, 9600)
+        self.ser = serial.Serial(port, 9600) # start arduino on given port with serial monitor on 9600 baud
 
     def readData(self):
-        return self.ser.readline().decode('utf-8')
+        return self.ser.readline().decode('utf-8') # read incoming line in utf-8 format
 
     def isAvailable(self):
-        return self.ser.isOpen()
+        return self.ser.isOpen() # return if serial monitor is open
 
     def writeData(self, data):
-        return self.ser.write(data.encode('utf-8'))
+        return self.ser.write(data.encode('utf-8')) # write given data in utf-8 format
 
     def kill(self):
-        self.ser.close()
+        self.ser.close() # close serial monitor
 
 class Graph(): # graph and GUI class for real time graphs
-    def __init__(self):
-        self.fig = plt.figure()
-        self.ax = self.fig.add_subplot(1, 1, 1)
+    def __init__(self): # constructor
+        self.fig = plt.figure() # create figure window
+        self.ax = self.fig.add_subplot(1, 1, 1) # add axes to figure
 
     def animate(self, i, x, y):
-        datIn = arduino.readData()
-        print(datIn)
-        if "," in datIn:
-            nx = float(datIn[0:datIn.index(",")])
-            if "-" in datIn[datIn.index(",")+1:]:
-                ny = float(datIn[datIn.index(",")+1:datIn.index(",")+11])
-            else:
-                ny = float(datIn[datIn.index(",")+1:datIn.index(",")+10])
-            print(nx)
-            print(ny)
-            x.append(nx)
-            y.append(ny)
+        datIn = arduino.readData() # receive data from arduino
+        print(datIn) # log data to computer printout
+        if "LX:" in datIn and "LY:" in datIn and "*" in datIn: # check if the message isn't garbled
+            # NOTE: -- chop off last dec to decrease accuracy (it was too accurate... moving while sitting still)
+            nx = float(datIn[datIn.index("LX:")+3:datIn.index("LY:")-1]) # get the new x coord of the location
+            ny = float(datIn[datIn.index("LY:")+3:datIn.index("*")-1]) # get y coord
+            print(nx) # log x coord
+            print(ny) # log y coord
+            x.append(nx) # add new x coord to x values
+            y.append(ny) # add new y coord to y values
             # self.ax.clear()
-            self.ax.plot(x, y)
+            self.ax.plot(x, y) # plot x and y data as lines
 
 def main(): # main function to run
     #execute code here
-    liveGraph = Graph()
-    ani = animation.FuncAnimation(liveGraph.fig, liveGraph.animate, fargs=(x, y), interval=15)
-    plt.show()
+    liveGraph = Graph() # create graph
+    ani = animation.FuncAnimation(liveGraph.fig, liveGraph.animate, fargs=(x, y), interval=15) # handles making it in "real time"
+    plt.show() # show graph
 
 arduino = ArduinoCommunicator("/dev/ttyUSB0"); # set up arduino on corresponding port
 main(); # run main function
--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,29 @@
 /*
  * Program: Arduino receiver
  * Author: Jordan Martin
- * Version: 1.0
+ * Version: 1.1
  * Date Created: 5 March 2019
  * Description: Receives information from the payload through a radio
  * Last Edited By: Jordan Martin
- * Last Edited: 11 March 2019
- * Reason Edited: Data testing for python graphs
+ * Last Edited: 12 March 2019
+ * Reason Edited: Added checksum
  */
-RF24 radio(7, 8);
+RF24 radio(7, 8); // CE = 7, CSN = 8
 const byte address[6] = "00001";
-
+bool checksum(String line) {
+  float lat = atof(line.substring(line.indexOf("LX:")+3, line.indexOf("LY:")).c_str());
+  float lng = atof(line.substring(line.indexOf("LY:")+3, line.indexOf("*")).c_str());
+  char toCheck[100] = "";
+  double temp = lat + lng;
+  int sum = 0;
+  for(char i : String(temp)){
+    sum += i;
+  }
+  sum %= 0xff;
+  sprintf(toCheck, "*%X", sum);
+  if(line.substring(line.indexOf("*"), line.length()).equalsIgnoreCase(toCheck)) return true;
+  return false;
+}
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -23,20 +36,10 @@ void setup() {
   radio.startListening(); // start receiver on radio
   Serial.println("Listening...");
 }
-
-float x = 10.2, y = 18.7;
 void loop() {
   while(radio.available()){ // continue while the radio is active
     char datIn[100] = ""; // create data in buffer
     radio.read(&datIn, sizeof(datIn)); // obtain incoming data
-    Serial.println(datIn); // log incoming data to serial monitor
+    if(checksum(datIn)) Serial.println(datIn); // log incoming data to serial monitor
   }
-  // x += 0.3;
-  // y = 2*y;
-  // String blah = String(x, sizeof(x)/sizeof(float)) + "," + String(y, sizeof(y)/sizeof(float));
-  // char text[100] = "";
-  // for(int i = 0; i < blah.length(); i++) {
-  //   text[i] = blah.charAt(i);
-  // }
-  // Serial.println(text);
 }
