4e9e64b876c1c2d9e99f20b4cdfed79c801ccfef
jeremymdunne@gmail.com <jeremymdunne@gmail.com>
2019-05-08 22:24:03 -0400

    working on radio testing... should be good on this end already

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ style.use("ggplot")
 import tkinter as tk
 from tkinter import ttk
 from tkinter import *
+from math import *
+from math import sqrt
+from time import sleep
 
 ###
 # Program: Arduino communication
@@ -22,11 +25,12 @@ from tkinter import *
 # Date Created: 8 March 2019
 # Description: Used to communicate between the arduinos and the computer... will eventually graph real time data
 # Last Edited By: Jordan Martin
-# Last Edited: 12 March 2019
-# Reason Edited: Weird data sort of filtered... added checksum
+# Last Edited: 8 May 2019
+# Reason Edited: Added radio testing
 ###
 #global variables here
 LARGE_FONT = ("Verdana", 12)
+curPage = 0
 
 # custom classes/functions here
 class ArduinoCommunicator(): # class to communicate with plugged in arduino through serial monitor
@@ -46,18 +50,17 @@ class ArduinoCommunicator(): # class to communicate with plugged in arduino thro
         self.ser.close() # close serial monitor
 
 class Graph(): # graph and GUI class for real time graphs
-    altitude = []
-    altTs = []
-    acceleration = []
-    velocity = [0]
-    xyAccel = []
-    xyVel = [0]
-    velTs = []
+    altitude = [0]
+    altTs = [0]
+    acceleration = [[0, -9.8, 0, 9.8]]
+    velocity = [[10, 20, 0, 22.36]]
+    velTs = [0]
     x = []
     y = []
-    distance = [0]
-    trajDeltaAlt = [0]
+    distance = [[0, 0]]
+    trajDeltaAlt = [[0, 0]]
     curTrajPos = 0
+    curDatPos = 0
     def __init__(self): # constructor
         self.velFig = plt.figure(1) # create figure window
         self.velAx = self.velFig.add_subplot(211) # add axes to figure
@@ -78,60 +81,102 @@ class Graph(): # graph and GUI class for real time graphs
         return True
 
     def predictRemaindingTrajectory(self):
-        print("predict trajectory here")
-
+        if len(self.trajDeltaAlt) > 1 and len(self.distance) > 1:
+            # vx = self.velocity[len(self.velocity)-1][0]
+            # vy = self.velocity[len(self.velocity)-1][1]
+            # print(self.trajDeltaAlt[self.curTrajPos][1])
+            # print(self.trajDeltaAlt[self.curTrajPos-1][1])
+            theta = atan2((self.trajDeltaAlt[self.curTrajPos][0] - self.trajDeltaAlt[self.curTrajPos-1][0]), (self.distance[self.curTrajPos][0] - self.distance[self.curTrajPos-1][0]))
+            vx = self.velocity[self.curTrajPos][3] * cos(theta)
+            vy = self.velocity[self.curTrajPos][3] * sin(theta)
+            # print(vy)
+                # vy = (self.trajDeltaAlt[self.curTrajPos][0] - self.trajDeltaAlt[self.curTrajPos-1][0]) / (self.trajDeltaAlt[self.curTrajPos][1] - self.trajDeltaAlt[self.curTrajPos-1][1])
+                # vx = (self.distance[self.curTrajPos][0] - self.distance[self.curTrajPos-1][0]) / (self.distance[self.curTrajPos][1] - self.distance[self.curTrajPos-1][1])
+            t = (self.distance[self.curTrajPos][0] - self.distance[0][0]) / vx
+            newAlt = self.trajDeltaAlt[self.curTrajPos][0] + vy * t - 0.5 * 9.8 * t**2
+            while newAlt > 0:
+                newDist = self.distance[self.curTrajPos][0] + vx * t
+                newAlt = self.trajDeltaAlt[self.curTrajPos][0] + vy * t - 0.5 * 9.8 * t**2
+                self.distance.append([newDist, t])
+                self.trajDeltaAlt.append([newAlt, t])
+                t += 0.025
 
     def updateTrajectory(self):
-        if len(self.xyVel) > 1 and len(self.velTs) > 1 and len(self.altitude) > 1:
-            newDist = 0.5 * (self.velTs[len(self.velTs)-1] - self.velTs[len(self.velTs)-2]) * (self.xyVel[len(self.xyVel)-1]+self.xyVel[len(self.xyVel)-2])
-            self.distance.append(newDist)
-            deltaAlt = self.altitude[len(self.altitude)-1] - self.altitude[len(altitude)-2]
-            self.trajDeltaAlt.append(deltaAlt)
-            self.curTrajPos += 1
-            while(len(self.distance)-1 > self.curTrajPos):
-                self.distance.pop()
-            while(len(self.trajDeltaAlt)-1 > self.curTrajPos):
-                self.trajDeltaAlt.pop()
-            self.predictRemaindingTrajectory()
+        if(self.trajDeltaAlt[self.curTrajPos][0] >= 0):
             self.trajAx.clear()
-            self.trajAx.plot(self.distance, self.trajDeltaAlt)
-
+            self.predictRemaindingTrajectory()
+            x = []
+            for i in self.distance:
+                x.append(i[0])
+            y = []
+            for i in self.trajDeltaAlt:
+                y.append(i[0])
+            self.trajAx.plot(x, y)
 
     def animate(self, i):
-        datIn = arduino.readData() # receive data from arduino
-        print(datIn) # log data to computer printout
-        if "AX:" in datIn and "AY:" in datIn and "AZ:" in datIn and "TS:" in datIn and "*" in datIn: # check if the message isn't garbled and is location
+        datIn = arduino.readData()
+        # print(datIn) # log data to computer printout
+        if "AX:" in datIn and "AY:" in datIn and "AZ:" in datIn and "TS:" in datIn and "*" in datIn and (curPage == 3 or curPage == 5): # check if the message isn't garbled and is location
             # FIND VELOCITY VECTOR
             xAccel = float(datIn[datIn.index("AX:")+3:datIn.index("AY:")])
-            yAccel = float(datIn[datIn.index("AY:")+3:datIn.index("AZ:")])
+            yAccel = float(datIn[datIn.index("AY:")+3:datIn.index("AZ:")]) - 9.8
             zAccel = float(datIn[datIn.index("AZ:")+3:datIn.index("TS:")])
             ts = float(datIn[datIn.index("TS:")+3:datIn.index("*")])
             self.velTs.append(ts)
-            accel = sqrt(xAccel ** 2 + yAccel ** 2 + zAccel ** 2)
-            self.xyAccel.append(sqrt(xAccel**2 + yAccel**2))
+            mag = sqrt(xAccel**2 + yAccel**2 + zAccel**2)
+            accel = [xAccel, yAccel, zAccel, mag]
             self.acceleration.append(accel)
             if len(self.acceleration) > 1:
-                newVel = 0.5 * (self.velTs[len(self.velTs)-1] - self.velTs[len(self.velTs)-2]) * (self.acceleration[len(self.acceleration)-1]+self.acceleration[len(self.acceleration)-2]) # trapezoidal area
+                vx = 0.5 * (self.velTs[len(self.velTs)-1] - self.velTs[len(self.velTs)-2]) * (self.acceleration[len(self.acceleration)-1][0] + self.acceleration[len(self.acceleration)-2][0]) + self.velocity[len(self.velocity)-1][0]
+                vy = 0.5 * (self.velTs[len(self.velTs)-1] - self.velTs[len(self.velTs)-2]) * (self.acceleration[len(self.acceleration)-1][1] + self.acceleration[len(self.acceleration)-2][1]) + self.velocity[len(self.velocity)-1][1]
+                vz = 0.5 * (self.velTs[len(self.velTs)-1] - self.velTs[len(self.velTs)-2]) * (self.acceleration[len(self.acceleration)-1][2] + self.acceleration[len(self.acceleration)-2][2]) + self.velocity[len(self.velocity)-1][2]
+                print(vy)
+                mag = sqrt(vx**2 + vy**2 + vz**2)
+                newVel = [vx, vy, vz, mag]
                 self.velocity.append(newVel)
-            if len(self.xyAccel) > 1:
-                newXYVel = 0.5 * (self.velTs[len(self.velTs)-1] - self.velTs[len(self.velTs)-2]) * (self.xyAccel[len(self.xyAccel)-1]+self.xyAccel[len(self.xyAccel)-2]) # trapezoidal area
+            velMag = []
+            for i in self.velocity:
+                velMag.append(i[3])
             self.velAx.clear()
-            self.velAx.plot(self.velTs, self.velocity)
-        elif "PA:" in datIn and "TS:" in datIn and "*" in datIn: # check if message isn't garbled and is altitude
+            self.velAx.plot(self.velTs, velMag)
+        elif "PA:" in datIn and "TS:" in datIn and "*" in datIn and (curPage == 3 or curPage == 5): # check if message isn't garbled and is altitude
             nAlt = float(datIn[datIn.index("PA:")+3:datIn.index("TS:")]) # get new altitude
             ts = float(datIn[datIn.index("TS:")+3:datIn.index("*")]) # get new timestamp
-            print(nAlt) # log altitude
-            print(ts) # log timestamp
-            self.altitude.append(nAlt) # add new altitude to altitude list
-            self.altTs.append(ts) # add timestamp to timestamp list
+            temp = str(ts)
+            if len(temp) < 4:
+                self.altitude.append(nAlt) # add new altitude to altitude list
+                self.altTs.append(ts) # add timestamp to timestamp list
+            if len(self.velocity) > 1 and len(self.velTs) > 1 and len(self.altitude) > 1 and len(temp) < 4:
+                xyVel = []
+                for i in self.velocity:
+                    xyVel.append(sqrt(i[0]**2 + i[1]**2))
+                    # print(xyVel[len(xyVel)-1])
+                # deltaAlt = self.altitude[len(self.altitude)-1] - self.altitude[0]
+                # self.trajDeltaAlt.append([deltaAlt, ts])
+                # print(self.distance[len(self.distance)-1][0])
+                newDist = 0.5 * (self.velTs[len(self.velTs)-1] - self.velTs[len(self.velTs)-2]) * (xyVel[len(xyVel)-1]+xyVel[len(xyVel)-2]) + self.distance[self.curTrajPos][0]
+                while((len(self.distance)-1) > self.curTrajPos):
+                    self.distance.pop()
+                while((len(self.trajDeltaAlt)-1) > self.curTrajPos):
+                    self.trajDeltaAlt.pop()
+                self.distance.append([newDist, ts])
+                self.trajDeltaAlt.append([nAlt, ts])
+                self.curTrajPos += 1
+                self.trajAx.clear()
+                x = []
+                for i in self.distance:
+                    x.append(i[0])
+                y = []
+                for i in self.trajDeltaAlt:
+                    y.append(i[0])
+                self.trajAx.plot(x, y)
+                self.updateTrajectory()
             self.altAx.clear() # clear prev alt graph
             self.altAx.plot(self.altTs, self.altitude) # plot new alt graph
         elif "LX:" in datIn and "LY:" in datIn and "*" in datIn: # check if the message isn't garbled and is location
             # NOTE: -- chop off last dec to decrease accuracy (it was too accurate... moving while sitting still)
             nx = float(datIn[datIn.index("LX:")+3:datIn.index("LY:")-1]) # get the new x coord of the location
             ny = float(datIn[datIn.index("LY:")+3:datIn.index("*")-1]) # get y coord
-            print(nx) # log x coord
-            print(ny) # log y coord
             self.x.append(nx) # add new x coord to x values
             self.y.append(ny) # add new y coord to y values
             goodLoc = checkIfGoodLocation(self.x, self.y) # TODO: test
@@ -152,11 +197,68 @@ class Application(tk.Tk):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew")
-        self.show_frame(StartPage)
+        self.show_frame(StartPage, 0)
 
-    def show_frame(self, cont):
+    def show_frame(self, cont, page):
+        global curPage
+        curPage = page
         frame = self.frames[cont]
         frame.tkraise()
+
+    def sync(self):
+        datIn = ""
+        while(arduino.isAvailable() and "SYNCED" not in datIn):
+            datIn = arduino.readData()
+            arduino.writeData("SYNC_NOW")
+
+    def arm(self):
+        datIn = ""
+        while(arduino.isAvailable() and "ARMED" not in datIn):
+            datIn = arduino.readData()
+            arduino.writeData("ARM")
+
+    def launch(self):
+        datIn = ""
+        while(arduino.isAvailable() and "STARTING_COUNTDOWN" not in datIn):
+            datIn = arduino.readData()
+            arduino.writeData("LAUNCH")
+
+    def abort(self):
+        datIn = ""
+        while(arduino.isAvailable() and "ABORTED" not in datIn):
+            datIn = arduino.readData()
+            arduino.writeData("ABORT")
+
+    def deployChute(self):
+        datIn = ""
+        while(arduino.isAvailable() and "DEPLOYING_CHUTE" not in datIn):
+            datIn = arduino.readData()
+            arduino.writeData("DEPLOY_CHUTE")
+
+    def toggleCamera(self):
+        datIn = ""
+        while(arduino.isAvailable() and "TOGGLED_CAMERA" not in datIn):
+            datIn = arduino.readData()
+            arduino.writeData("TOGGLE_CAMERA")
+
+    def writeToSD(self):
+        datIn = ""
+        while(arduino.isAvailable() and "SD_WRITTEN" not in datIn):
+            datIn = arduino.readData()
+            arduino.writeData("WRITE_SD")
+
+    def receiveGraphData(self):
+        datIn = ""
+        while(arduino.isAvailable() and "GRAPH_DATA_SENT" not in datIn):
+            datIn = arduino.readData()
+            arduino.writeData("REQUESTING_GRAPH_DATA")
+
+    def toggleBuzzer(self):
+        datIn = ""
+        while(arduino.isAvailable() and "BUZZER_TOGGLED" not in datIn):
+            datIn = arduino.readData()
+            arduino.writeData("TOGGLE_BUZZER")
+
 
 class StartPage(tk.Frame):
     def __init__(self, parent, controller):
@@ -164,19 +266,19 @@ class StartPage(tk.Frame):
         label = tk.Label(self, text="Start Page", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
         button = ttk.Button(self, text="Control Page",
-                            command=lambda: controller.show_frame(PageOne))
+                            command=lambda: controller.show_frame(PageOne, 1))
         button.pack()
-        button2 = ttk.Button(self, text="Visit Page 2",
-                            command=lambda: controller.show_frame(PageTwo))
+        button2 = ttk.Button(self, text="Test Page",
+                            command=lambda: controller.show_frame(PageTwo, 2))
         button2.pack()
         button3 = ttk.Button(self, text="Graph Page",
-                            command=lambda: controller.show_frame(PageThree))
+                            command=lambda: controller.show_frame(PageThree, 3))
         button3.pack()
         button4 = ttk.Button(self, text="Graph Page 2",
-                            command=lambda: controller.show_frame(PageFour))
+                            command=lambda: controller.show_frame(PageFour, 4))
         button4.pack()
         button5 = ttk.Button(self, text="Graph Page 3",
-                            command=lambda: controller.show_frame(PageFive))
+                            command=lambda: controller.show_frame(PageFive, 5))
         button5.pack()
 
 class PageOne(tk.Frame):
@@ -185,7 +287,7 @@ class PageOne(tk.Frame):
         label = tk.Label(self, text="Control Page!", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
         button1 = ttk.Button(self, text="Back to Home",
-                            command=lambda: controller.show_frame(StartPage))
+                            command=lambda: controller.show_frame(StartPage, 0))
         button1.pack(side=TOP)
         button2 = Button(self, text="SYNC RADIO", font=("Verdana", 24), width=10, command=lambda:controller.sync())
         button2.place(relx=0.25, rely=0.35, anchor=CENTER)
@@ -199,11 +301,21 @@ class PageOne(tk.Frame):
 class PageTwo(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Control Page!", font=LARGE_FONT)
+        label = tk.Label(self, text="Test Page!", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
         button1 = ttk.Button(self, text="Back to Home",
-                            command=lambda: controller.show_frame(StartPage))
+                            command=lambda: controller.show_frame(StartPage, 0))
         button1.pack(side=TOP)
+        button2 = Button(self, text="DEPLOY PARACHUTE", font=("Verdana", 24), width=10, command=lambda:controller.deployChute())
+        button2.place(relx=0.25, rely=0.35, anchor=CENTER)
+        button3 = Button(self, text="TOGGLE CAMERA", font=("Verdana", 24), width=10, command=lambda:controller.toggleCamera())
+        button3.place(relx=0.75, rely=0.35, anchor=CENTER)
+        button4 = Button(self, text="WRITE TO SD", font=("Verdana", 24), width=10, command=lambda:controller.writeToSD())
+        buttom4.place(relx=0.15, rely=0.7, anchor=CENTER)
+        button5 = Button(self, text="RECEIVE GRAPH DATA", font("Verdana", 24), width=10, command=lambda:controller.receiveGraphData())
+        button5.place(relx=0.5, rely=0.7, anchor=CENTER)
+        button6 = Button(self, text="TOGGLE BUZZER", font("Verdana", 24), width=10, command=lambda:controller.toggleBuzzer())
+        button6.place(relx=0.85, rely=0.7, anchor=CENTER)
 
 class PageThree(tk.Frame):
     def __init__(self, parent, controller):
@@ -211,7 +323,7 @@ class PageThree(tk.Frame):
         label = tk.Label(self, text="Graph Page!", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
         button1 = ttk.Button(self, text="Back to Home",
-                            command=lambda: controller.show_frame(StartPage))
+                            command=lambda: controller.show_frame(StartPage, 0))
         button1.pack()
         canvas = FigureCanvasTkAgg(liveGraph.velFig, self)
         canvas.show()
@@ -226,7 +338,7 @@ class PageFour(tk.Frame):
         label = tk.Label(self, text="Graph Page!", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
         button1 = ttk.Button(self, text="Back to Home",
-                            command=lambda: controller.show_frame(StartPage))
+                            command=lambda: controller.show_frame(StartPage, 0))
         button1.pack()
         canvas = FigureCanvasTkAgg(liveGraph.locFig, self)
         canvas.show()
@@ -241,7 +353,7 @@ class PageFive(tk.Frame):
         label = tk.Label(self, text="Graph Page!", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
         button1 = ttk.Button(self, text="Back to Home",
-                            command=lambda: controller.show_frame(StartPage))
+                            command=lambda: controller.show_frame(StartPage, 0))
         button1.pack()
         canvas = FigureCanvasTkAgg(liveGraph.trajFig, self)
         canvas.show()
@@ -250,14 +362,9 @@ class PageFive(tk.Frame):
         toolbar.update()
         canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-def main(): # main function to run
-    #execute code here
-    ani = animation.FuncAnimation(liveGraph.velFig, liveGraph.animate, fargs=(), interval=15) # handles making it in "real time"
-    app = Application()
-    app.mainloop()
-    # plt.show() # show graph
-
-# arduino = ArduinoCommunicator("/dev/ttyUSB0"); # set up arduino on corresponding port
 liveGraph = Graph() # create graph
-main(); # run main function
+arduino = ArduinoCommunicator("/dev/ttyUSB0"); # set up arduino on corresponding port
+app = Application()
+ani = animation.FuncAnimation(liveGraph.trajFig, liveGraph.animate, interval=15)
+app.mainloop()
 # NOTE: should be able to just stick this into tkinter to add buttons for rocket tests/launches
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <SPI.h>
 #include <RF24.h>
 
 /*
@@ -8,11 +9,11 @@
  * Date Created: 5 March 2019
  * Description: Receives information from the payload through a radio
  * Last Edited By: Jordan Martin
- * Last Edited: 12 March 2019
- * Reason Edited: Added checksum
+ * Last Edited: 8 May 2019
+ * Reason Edited: Added radio testing
  */
 RF24 radio(7, 8); // CE = 7, CSN = 8
-const byte address[6] = "00001";
+const byte addresses[][6] = {"00001", "00002"};
 
 bool checksum(String line) {
   float x;
@@ -23,6 +24,16 @@ bool checksum(String line) {
   } else if(line.indexOf("PA:") >= 0) { // if format if for altitude
     x = atof(line.substring(line.indexOf("PA:")+3, line.indexOf("TS:")).c_str()); // get altitude
     y = atoi(line.substring(line.indexOf("TS:")+3, line.indexOf("*")).c_str()); // get timestamp
+  } else {
+    int sum = 0;
+    for(char i : line) {
+      if(line.indexOf("*") > 0) break;
+      sum += (int)i;
+    }
+    sum %= 0xff;
+    char toCheck[100] = "";
+    sprintf(toCheck, "*%X", sum);
+    if(line.substring(line.indexOf("*"), line.indexOf("/r")).equalsIgnoreCase(toCheck)) return true;
   }
   char toCheck[100] = "";
   double temp = x + y; // combine float values
@@ -40,15 +51,55 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   radio.begin(); // start radio comms
-  radio.openReadingPipe(0, address); // route radio to correct address
+  radio.openReadingPipe(0, addresses[1]); // route radio to correct address
+  radio.openWritingPipe(1, addresses[0]);
   radio.setPALevel(RF24_PA_MIN);
   radio.startListening(); // start receiver on radio
   Serial.println("Listening...");
 }
 void loop() {
+  radio.startListening();
   while(radio.available()){ // continue while the radio is active
+    char commandIn[100] = "";
     char datIn[100] = ""; // create data in buffer
+    while(Serial.available()) {
+      delay(3);
+      if(Serial.available() > 0) {
+        commandIn += (char)(Serial.read());
+      }
+    }
     radio.read(&datIn, sizeof(datIn)); // obtain incoming data
     if(checksum(datIn)) Serial.println(datIn); // log incoming data to serial monitor
+    else if(commandIn.substring(0, commandIn.indexOf("/r")).equalsIgnoreCase("ABORT")) {
+      Serial.println("ABORTED");
+    } else if(commandIn.substring(0, commandIn.indexOf("/r")).equalsIgnoreCase("SYNC")) {
+      Serial.println("SYNCED");
+    } else if(commandIn.substring(0, commandIn.indexOf("/r")).equalsIgnoreCase("ARM")) {
+      Serial.println("ARMED")
+    } else if(commandIn.substring(0, commandIn.indexOf("/r")).equalsIgnoreCase("LAUNCH")) {
+      Serial.println("STARTING_COUNTDOWN")
+    } else if(commandIn.substring(0, commandIn.indexOf("/r")).equalsIgnoreCase("WRITE_SD")) {
+      radio.stopListening();
+      char writeSD[] = "WRITE_SD";
+      radio.write(&writeSD, sizeof(writeSD));
+    } else if(commandIn.substring(0, commandIn.indexOf("/r")).equalsIgnoreCase("TOGGLE_BUZZER")) {
+      radio.stopListening();
+      char toggleBuzzer[] = "TOGGLE_BUZZER";
+      radio.write(&toggleBuzzer, sizeof(toggleBuzzer));
+    } else if(commandIn.substring(0, commandIn.indexOf("/r")).equalsIgnoreCase("TOGGLE_CAMERA")) {
+      radio.stopListening();
+      char toggleCamera[] = "TOGGLE_CAMERA";
+      radio.write(&toggleCamera, sizeof(toggleCamera));
+    } else if(commandIn.substring(0, commandIn.indexOf("/r")).equalsIgnoreCase("DEPLOY_CHUTE")) {
+      radio.stopListening();
+      char deployChute[] = "DEPLOY_CHUTE";
+      radio.write(&deployChute, sizeof(deployChute));
+    } else if(commandIn.substring(0, commandIn.indexOf("/r")).equalsIgnoreCase("REQUESTING_GRAPH_DATA")) {
+      radio.stopListening();
+      char requestGraphData[] = "REQUESTING_GRAPH_DATA";
+      radio.write(&requestGraphData, sizeof(requestGraphData));
+    }
+
+    radio.startListening();
   }
 }
